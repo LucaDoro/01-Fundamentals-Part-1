39d53c4dd798f71dccf1585ac7c356023e239003
LucaD <135154766+LucaDoro@users.noreply.github.com>
2023-08-07 19:11:48 +0200

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,91 @@
+// //* Assignment: Values and Variables
+
+// // let country = "Luxemburg";
+// // let continent = "Europe";
+// let population = 0.5;
+
+// // console.log(country);
+// // console.log(continent);
+// console.log(population);
+
+// //* Assignment: Data Types
+
+// // let isIsland = false;
+// let language;
+
+// // console.log(typeof isIsland);
+// console.log(typeof population);
+// // console.log(typeof country);
+// console.log(typeof language);
+
+// //* Assignment: let, const and var
+
+// language = 'luxemburgish';
+// const country = "Luxemburg";
+// const continent = "Europe";
+// const isIsland = false;
+// // isIsland = true;
+
+// //* Assignment: Basic Operators
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// let description1 =
+//   country +
+//   ' is in ' +
+//   continent +
+//   ', and its ' +
+//   population +
+//   ' million people speak ' +
+//   language;
+// console.log(description1);
+
+// //* Assignment: Strings and Templates Literals
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+
+// const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+// console.log(description2);
+
+// //* Assignment: If/else Statements
+
+// let population = 0.5;
+// // let population = 13;
+// // let population = 130;
+// let country = 'Luxemburg';
+// let average = 33;
+
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(`${country}'s population is ${average - population} million below average`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,197 @@
+
+// //* Lecture: Values and Variables
+
+// console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+
+// console.log("Luca");
+// console.log(23);
+
+// let firstName = "Matilda ";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonasMatilda = "LM";
+// let $function = 27;
+
+// let person = "luca";
+// let PI = 3.1415;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob);
+// console.log(myCurrentJob);
+
+// //* Lecture: Data Types
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Luca');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1999;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// //* Lecture: let, const and var
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1999;
+// // birthYear = 1990;
+// // const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'Doro';
+// console.log(lastName);
+
+// //* Lecture: Basic Operators
+
+// // Math operators
+// const now = 2037;
+// const ageLuca = now - 1999;
+// const ageSarah = now - 2018;
+// console.log(ageLuca, ageSarah);
+
+// console.log(ageLuca * 2, ageLuca / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Luca';
+// const lastName = 'Doro';
+// console.log(firstName + ' ' + lastName);
+
+
+// //* Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4 // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; //x = x - 1
+// x--;
+// console.log(x);
+
+// //* Comparison operators
+// console.log(ageLuca > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1999 > now - 2018);
+
+// //* Operator precedence
+
+// const now = 2037;
+// const ageLuca = now - 1999;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1999 > now - 2018);
+
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageLuca + ageSarah) / 2;
+// console.log(ageLuca, ageSarah, averageAge);
+
+// //* Strings and Templates Literals
+
+// const firstName = 'Luca';
+// const job = 'coder';
+// const birthYear = 1999;
+// const year = 2037;
+
+// const luca = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+// console.log(luca);
+
+// const lucaNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(lucaNew);
+
+// console.log(`Just a regular string...`);
+
+// // console.log('String with \n\
+// // multiple \n\
+// // lines');
+
+// console.log(`String with
+// multiple
+// lines`);
+
+// //* If/else Statements
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log('Sarah can start driving license 🚗');
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+// }
+
+// const birthYear = 1999;
+
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
